a07ff29065b461d2eac9b548d11ce49a550fd9ea
Chris Q <chris@1off.dev>
2022-06-29 20:42:59 -0700

    Add closing <\a> tag #4

--- check_store.py
+++ check_store.py
@@ -4,7 +4,7 @@ from notify import pushNotify
 
 STORE_URL = "https://www.apple.com/shop/buy-mac/macbook-air/midnight-apple-m2-chip-with-8-core-cpu-and-10-core-gpu-512gb"
 
-MESSAGE = f"<a href='{STORE_URL}'>Buy NOW!"
+MESSAGE = f"<a href='{STORE_URL}'>Buy NOW!</a>"
 PAGE_SCRIPT_VARIABLE = "window.pageLevelData.CTO_BOOTSTRAP"
 EXECUTE_SCRIPT = f"return {PAGE_SCRIPT_VARIABLE}"
 
